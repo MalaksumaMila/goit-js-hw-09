2d455482b8463e51d35f07ba801e4edc8a8c8a8c
MalaksumaMila <malaksumaaa@gmail.com>
2025-07-15 11:47:37 +0000

    Deploying to gh-pages from @ MalaksumaMila/goit-js-hw-09@c03594ed4382b280065bbe805262da1c13546494 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const e={email:"",message:""},r="feedback-form-state",m=document.querySelector(".feedback-form");m.addEventListener("input",t=>{const{name:o,value:a}=t.target;e[o]=a,localStorage.setItem(r,JSON.stringify(e))});
+//# sourceMappingURL=2-form.js.map
